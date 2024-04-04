d2780bad4cab5f36e25d80f6e62e03dea7cc72d1
Javi <javii.oteero@gmail.com>
2024-04-04 13:53:37 +0200

    todos los tests pasados

--- src/es/iesmz/tests/Vocales2Test.java
+++ src/es/iesmz/tests/Vocales2Test.java
@@ -9,4 +9,24 @@ class Vocales2Test {
     void sinvocalesTest() {
         assertEquals("Hl Mnd", Vocales2.sinvocales("Hola Mundo"));
     }
+
+    @Test
+    public void testSinvocales2() {
+        assertEquals("st s n cdn", Vocales2.sinvocales("Esto Es una cadenA"));
+    }
+
+    @Test
+    public void testSinvocales3() {
+        assertEquals("Jv ml Mglln", Vocales2.sinvocales("Java mola MogollOn"));
+    }
+
+    @Test
+    public void testSinvocales4() {
+        assertEquals("4h0r4 s0n nm3r0s", Vocales2.sinvocales("4h0r4 s0n num3r0s"));
+    }
+
+    @Test
+    public void testSinvocales5() {
+        assertEquals("Cdn Sn Vcls", Vocales2.sinvocales("Cadena Sin Vocales"));
+    }
 }
